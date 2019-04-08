7b2e395c1576ea52b3ce3df2310cc2cd552d9334
boram lamppa <yangboram@hotmail.com>
2019-04-08 12:39:48 -0400

    Modified the code so the test database now displays the people in the personnel table.

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'boraml07_test',
+        'username' => 'boraml07_test',
+        'password' => 'Whip-1234',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    
+    ]
+    ];
+
+    
